4c8273f6e39ae58f894ae231d77b98ec4861bba6
EllaKamenetsky <kamenetsky@eneriqs.com>
2023-08-07 13:10:31 +0300

    Validation

--- Entity/Validators/LifeValidator.cs
+++ Entity/Validators/LifeValidator.cs
@@ -3,7 +3,7 @@ public class LifeValidator : AbstractValidator<LifePolicy>
 {
     public LifeValidator()
     {
-        RuleFor(x => x.DateOfBirth).NotEqual(DateTime.MinValue).WithMessage("Life policy must include Date of Birth.").GreaterThanOrEqualTo(DateTime.Today.AddYears(-100)).WithMessage("Max eligible age for coverage is 100 years.");
+        RuleFor(x => x.DateOfBirth).Equal(DateTime.MinValue).WithMessage("Life policy must include Date of Birth.").LessThanOrEqualTo(DateTime.Today.AddYears(-100)).WithMessage("Max eligible age for coverage is 100 years.");
         RuleFor(x => x.Amount).NotEqual(0).WithMessage("Life policy must include an Amount.");
     }
 
